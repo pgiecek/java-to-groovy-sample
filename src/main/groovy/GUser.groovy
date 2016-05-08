14594645dbdf5801aedcc7ccaf11c44ee1ff887d
Petr Giecek <petr.giecek.82@gmail.com>
2016-05-08 22:02:10 +0200

    1: Remove semicolons

--- src/main/groovy/GUser.groovy
+++ src/main/groovy/GUser.groovy
@@ -3,32 +3,32 @@ import groovy.transform.TypeChecked
 @TypeChecked
 public class GUser implements Comparable<GUser> {
 
-    private final String firstName;
+    private final String firstName
 
-    private final String lastName;
+    private final String lastName
 
-    private final int birthYear;
+    private final int birthYear
 
     private GUser(Builder builder) {
-        this.firstName = builder.firstName;
-        this.lastName = builder.lastName;
-        this.birthYear = builder.birthYear;
+        this.firstName = builder.firstName
+        this.lastName = builder.lastName
+        this.birthYear = builder.birthYear
     }
 
     public String getFirstName() {
-        return firstName;
+        return firstName
     }
 
     public String getLastName() {
-        return lastName;
+        return lastName
     }
 
     public int getBirthYear() {
-        return birthYear;
+        return birthYear
     }
 
     public static Builder builder() {
-        return new Builder();
+        return new Builder()
     }
 
     @Override
@@ -37,12 +37,12 @@ public class GUser implements Comparable<GUser> {
             return 0;
         }
 
-        int result = this.lastName.compareTo(o.lastName);
+        int result = this.lastName.compareTo(o.lastName)
         if (result != 0) {
-            return result;
+            return result
         }
 
-        return this.firstName.compareTo(o.firstName);
+        return this.firstName.compareTo(o.firstName)
     }
 
     @Override
@@ -52,18 +52,18 @@ public class GUser implements Comparable<GUser> {
 
         GUser user = (GUser) o;
 
-        if (birthYear != user.birthYear) return false;
-        if (firstName != null ? !firstName.equals(user.firstName) : user.firstName != null) return false;
-        return lastName != null ? lastName.equals(user.lastName) : user.lastName == null;
+        if (birthYear != user.birthYear) return false
+        if (firstName != null ? !firstName.equals(user.firstName) : user.firstName != null) return false
+        return lastName != null ? lastName.equals(user.lastName) : user.lastName == null
 
     }
 
     @Override
     public int hashCode() {
-        int result = firstName != null ? firstName.hashCode() : 0;
-        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
-        result = 31 * result + birthYear;
-        return result;
+        int result = firstName != null ? firstName.hashCode() : 0
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0)
+        result = 31 * result + birthYear
+        return result
     }
 
     @Override
@@ -72,34 +72,34 @@ public class GUser implements Comparable<GUser> {
                 "firstName:" + firstName +
                 ", lastName:" + lastName +
                 ", birthYear:" + birthYear +
-                ')';
+                ')'
     }
 
     public static class Builder {
 
-        private String firstName;
+        private String firstName
 
-        private String lastName;
+        private String lastName
 
-        private int birthYear;
+        private int birthYear
 
         public Builder firstName(String firstName) {
-            this.firstName = firstName;
-            return this;
+            this.firstName = firstName
+            return this
         }
 
         public Builder lastName(String lastName) {
-            this.lastName = lastName;
-            return this;
+            this.lastName = lastName
+            return this
         }
 
         public Builder birthYear(int birthYear) {
-            this.birthYear = birthYear;
-            return this;
+            this.birthYear = birthYear
+            return this
         }
 
         public GUser build() {
-            return new GUser(this);
+            return new GUser(this)
         }
     }
 }
