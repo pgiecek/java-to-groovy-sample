3408549496fdc330d5103f78e224b3bc322887f2
Petr Giecek <petr.giecek.82@gmail.com>
2016-05-12 20:30:52 +0200

    8: Replace builder with @Builder transformation

--- src/main/groovy/GUser.groovy
+++ src/main/groovy/GUser.groovy
@@ -2,54 +2,18 @@ import groovy.transform.EqualsAndHashCode
 import groovy.transform.Sortable
 import groovy.transform.ToString
 import groovy.transform.TypeChecked
+import groovy.transform.builder.Builder
 
 @TypeChecked
 @ToString(includeNames = true)
 @EqualsAndHashCode
 @Sortable(includes = ['lastName', 'firstName'])
+@Builder
 class GUser {
 
-    final String firstName
+    String firstName
 
-    final String lastName
+    String lastName
 
-    final int birthYear
-
-    private GUser(Builder builder) {
-        this.firstName = builder.firstName
-        this.lastName = builder.lastName
-        this.birthYear = builder.birthYear
-    }
-
-    static Builder builder() {
-        new Builder()
-    }
-
-    static class Builder {
-
-        private String firstName
-
-        private String lastName
-
-        private int birthYear
-
-        Builder firstName(String firstName) {
-            this.firstName = firstName
-            this
-        }
-
-        Builder lastName(String lastName) {
-            this.lastName = lastName
-            this
-        }
-
-        Builder birthYear(int birthYear) {
-            this.birthYear = birthYear
-            this
-        }
-
-        GUser build() {
-            new GUser(this)
-        }
-    }
+    int birthYear
 }
